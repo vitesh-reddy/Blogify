a8e34ce745dd57ba5e9d841566639d1d01d1b319
K_L_Vitesh_Reddy <147067995+vitesh-reddy@users.noreply.github.com>
2025-05-23 14:29:22 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://blogify-backend-5dga.onrender.com';
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
