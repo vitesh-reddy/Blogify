f735b4bc88e762fd9d5aeb16c5938265cf1fc0e3
K_L_Vitesh_Reddy <147067995+vitesh-reddy@users.noreply.github.com>
2025-05-24 19:31:43 +0530

    updated to use BACKEND_URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000/api';
+const API_URL = `${import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000'}/api`;
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
